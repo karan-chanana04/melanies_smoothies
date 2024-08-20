97179a1abcb6362ec9423e9e8fd724e6419171e7
karan-chanana04 <58719465+karan-chanana04@users.noreply.github.com>
2024-08-20 16:46:37 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
+streamlit.title("My parents neew Healthy Diner")
+
 # Write directly to the app
 st.title(":cup_with_straw: Customize your Smoothie! :cup_with_straw:")
 st.write(
