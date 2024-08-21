4984e46581bfd3bcb36e122769509633f063e3eb
karan-chanana04 <58719465+karan-chanana04@users.noreply.github.com>
2024-08-20 17:09:11 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,7 @@
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
-
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
 
 #streamlit.title("My parents neew Healthy Diner")
 
@@ -46,6 +43,9 @@ if ingredients_list:
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
     #st.write(my_insert_stmt)
     time_to_insert = st.button("Submit Order")
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+    st.text(fruityvice_response.json())
+
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
